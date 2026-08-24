cf95efb88b1cbcee54abde53b9d3ade975fbee4b
katharinagudat-hash <katharina.gudat@gmail.com>
2026-08-24 10:49:31 +0200

    ...

--- content/projekte/Explainable AI/analyse.py
+++ content/projekte/Explainable AI/analyse.py
@@ -5,7 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 #import lime.lime_tabular
+import sys
+from pathlib import Path
+import plotly.graph_objects as go
 
-data = pd.read_csv('penguins.csv')
+ORDNER = Path(__file__).parent
+if str(ORDNER) not in sys.path:
+    sys.path.insert(0, str(ORDNER))
+
+data = pd.read_csv(ORDNER/'penguins.csv')
 data = data.dropna() # removes rows with NaN values. 333 datapoints remain.
 
